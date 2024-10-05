8c0b0e4d84a4b4f6665cad5bdf996d732e7ff0f3
Jeanine Bonot <jeaninebonot@gmail.com>
2024-10-06 01:12:09 +0200

    feat: parse data into excel output

--- extract_pdf_text.py
+++ extract_pdf_text.py
@@ -1,8 +1,24 @@
-import PyPDF2 # type: ignore
+import PyPDF2
 import os
+from format_text import parse_doc
+import argparse
+from datetime import datetime
 
 input_folder_path = "input"
 output_folder_path = "output"
+
+parser = argparse.ArgumentParser(description='Check for debug flag.')
+parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+args = parser.parse_args()
+
+os.makedirs(output_folder_path, exist_ok=True)
+
+if args.debug:
+    debug_path = os.path.join(output_folder_path, "debug")
+    os.makedirs(debug_path, exist_ok=True)
+
+now = datetime.now()
+timestamp = now.strftime("%Y%m%d_%H%M%S")  # Format: YYYYMMDD_HHMMSS
 
 # Function to extract text from a PDF
 def extract_text_from_pdf(pdf_file):
@@ -22,14 +38,21 @@ for filename in os.listdir(input_folder_path):
     if not (os.path.isfile(pdf_file_path) and pdf_file_path.endswith(".pdf")):
         continue
 
-    txt_file_path = os.path.join(output_folder_path, filename[:-4] + ".txt")
+    txt_file_path = os.path.join(output_folder_path, f"output_{timestamp}.txt")
 
-    # Extract the text and write to .txt
     with open(pdf_file_path, "rb") as pdf_file:
         extracted_text = extract_text_from_pdf(pdf_file)
-    with open(txt_file_path, "w", encoding="utf-8") as txt_file:
-        txt_file.write(extracted_text)
+    
+    if args.debug:
+        debug_file_path = os.path.join(debug_path, filename[:-4] + ".txt")   
+        with open(debug_file_path, "w", encoding="utf-8") as txt_file:
+            txt_file.write(extracted_text)
+        print(f"DEBUG: Extrated text from \"{filename}\" to \"{debug_file_path}\"")
 
-    print(f"Extrated text from \"{filename}\" to \"{txt_file_path}\"")
+    excel_line = parse_doc(filename[:-4], extracted_text)
+    with open(txt_file_path, 'a') as file:
+        file.write(excel_line + "\n")
+
+    print(f"Extrated data from \"{filename}\" to \"{txt_file_path}\"")
 
 
---
+++ format_text.py
@@ -0,0 +1,91 @@
+from enum import Enum
+from datetime import datetime
+import re
+
+date_pattern = r"\b(\d{1,2}/\d{1,2}/\d{4})\b"
+
+class COLUMN(Enum):
+    LAST_NAME = "naam"
+    FIRST_NAME = "voornaam"
+    GENDER = "geslacht"
+    DOB = "geboortedatum"
+    SURGERY_DATE = "datum_ingreep"
+    JOINT = "gewricht"
+    PROCEDURE = "ingreep"
+    SIDE = "zijde"
+    SUPERVISOR = "supervisor"
+    SURGEON_ASSISTANT = "chirug_asssistent"
+    MATERIAL_COMPANY = "firma_material"
+
+order = [
+    COLUMN.LAST_NAME,
+    COLUMN.FIRST_NAME,
+    COLUMN.GENDER,
+    COLUMN.DOB,
+    COLUMN.SURGERY_DATE,
+    COLUMN.JOINT,
+    COLUMN.PROCEDURE,
+    COLUMN.SIDE,
+    COLUMN.SUPERVISOR,
+    COLUMN.SURGEON_ASSISTANT,
+    COLUMN.MATERIAL_COMPANY
+]
+
+def parse_name_list(lines, start_index):
+    def clean_name(full_name):
+        # Remove the dash and extra spaces
+        parts = full_name.strip().replace("-", "").split()
+
+        # If the first part is a title, remove it
+        if parts[0].endswith('.'):
+            parts = parts[1:]
+
+        # Join all but the last part (which is assumed to be the first name) as the last name
+        return " ".join(parts[:-1]).title()
+
+    names = []
+    while start_index < len(lines) and lines[start_index].strip().startswith("-"):
+        full_name = lines[start_index].strip()  # Strip once for efficiency
+        last_name = clean_name(full_name)
+        names.append(last_name)
+        start_index += 1
+
+    return names
+
+def parse_line(line, lines, index):
+    def extract_value(line):
+        return line.split(":", 1)[1].strip()
+
+    match = re.search(date_pattern, line)
+    if match:
+        return COLUMN.SURGERY_DATE.value, match.group(1)
+    
+    if line.startswith("Ingreep"):
+        return COLUMN.PROCEDURE.value, extract_value(line)
+    
+    if line.startswith("Lateraliteit"):
+        return COLUMN.SIDE.value, extract_value(line).lower()
+    
+    if line.startswith("Chirugen"):
+        chirugen = parse_name_list(lines, index + 1)
+        return COLUMN.SUPERVISOR.value, ",".join(chirugen)
+    
+    return None, None
+
+
+def parse_doc(filename, text):
+    result = {column.value: "" for column in order}
+    parts = filename.split()
+    result[COLUMN.LAST_NAME.value] = parts[0].rstrip(',')
+    result[COLUMN.FIRST_NAME.value] = parts[1]
+    result[COLUMN.DOB.value] = datetime.strptime(parts[2], "%d.%m.%Y").strftime("%d/%m/%Y")
+
+    lines = text.splitlines()
+    for index, line in enumerate(lines):
+        cell, value = parse_line(line.strip(), lines, index)
+        if cell is None:
+            continue
+        result[cell] = value
+        
+    excel_line = "\t".join(result.get(column.value, "") for column in order)
+    return excel_line
