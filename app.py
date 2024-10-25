5588e74791a6663fc51ad54ce09260e45c588939
Jeanine Bonot <jeaninebonot@gmail.com>
2024-10-25 13:08:15 +0200

    docs: remove non-helpful comments

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ class FileReaderApp:
         self.root.title("PDF Report Extraction")
 
         # Set default window size (width x height)
-        self.root.geometry(
-            "850x275"
-        )  # Set the window size to 400 pixels wide and 300 pixels tall
+        self.root.geometry("850x275")
 
         # Create a button to select files
         self.select_button = tk.Button(
@@ -25,19 +23,15 @@ class FileReaderApp:
 
         # Create a scrolled text area to display file names
         self.text_area = scrolledtext.ScrolledText(root, width=100, height=10)
-        self.text_area.grid(
-            row=1, column=0, pady=10, sticky="nsew"
-        )  # Stretch to fill space
+        self.text_area.grid(row=1, column=0, pady=10, sticky="nsew")
 
         # Configure grid to allow resizing
-        root.grid_rowconfigure(1, weight=1)  # Allow row 1 to expand
-        root.grid_columnconfigure(0, weight=1)  # Allow column 0 to expand
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
 
         # Position button at the bottom right of the text area
         self.button_frame = tk.Frame(root)
-        self.button_frame.grid(
-            row=2, column=0, sticky="se"
-        )  # Place it below the text area
+        self.button_frame.grid(row=2, column=0, sticky="se")
 
         # Create a button to copy the text area contents
         self.copy_button = tk.Button(
@@ -46,9 +40,7 @@ class FileReaderApp:
             command=self.copy_to_clipboard,
             state=tk.DISABLED,
         )
-        self.copy_button.pack(
-            side=tk.RIGHT, padx=5
-        )  # Align it to the right within the frame
+        self.copy_button.pack(side=tk.RIGHT, padx=5)
 
         # Divider
         self.status_divider = tk.Frame(root, height=2, bd=1, bg="grey")
@@ -56,9 +48,7 @@ class FileReaderApp:
 
         # Label to show status
         self.status_label = tk.Label(root, text="Ready", fg="blue")
-        self.status_label.grid(
-            row=4, column=0, sticky="se", padx=10, pady=5
-        )  # Bottom right
+        self.status_label.grid(row=4, column=0, sticky="se", padx=10, pady=5)
 
         # Queue for communication between threads
         self.queue = queue.Queue()
@@ -75,10 +65,10 @@ class FileReaderApp:
         file_paths = filedialog.askopenfilenames(title="Select Files")
         self.check_text()
 
-        # Print the file names in the text area
         for index, file_path in enumerate(file_paths):
             if not (file_path.endswith(".pdf")):
                 continue
+
             self.status_label.config(
                 text=f"Processing {index + 1} of {len(file_paths)} files..."
                 " Please wait."
@@ -87,11 +77,9 @@ class FileReaderApp:
             with open(file_path, "rb") as pdf_file:
                 extracted_text = extract_text_from_pdf(pdf_file)
 
-            file_name = file_path.split("/")[-1]  # Get the file name from the full path
+            file_name = file_path.split("/")[-1]
             output = parse_doc(file_name[:-4], extracted_text)
-            self.text_area.insert(
-                tk.END, output + "\n"
-            )  # Insert file name into text area
+            self.text_area.insert(tk.END, output + "\n")
 
         # Reset the status label and cursor
         self.check_text()
