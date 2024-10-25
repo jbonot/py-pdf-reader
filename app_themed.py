d4c072f6d27e16473249d80c368d01848bc9b6da
Jeanine Bonot <jeaninebonot@gmail.com>
2024-10-25 17:19:41 +0200

    feat: use theme conventions

--- app_themed.py
+++ app_themed.py
@@ -12,42 +12,30 @@ from data_extractor.extract_scanned_pdf_text import extract_text_from_pdf
 class FileReaderApp(TKMT.ThemedTKinterFrame):
     def __init__(self):
         super().__init__("PDF Report Extraction", "park", "light")
-        self.root.geometry("850x275")
-
-        # Create a button to select files
-        self.select_button = ttk.Button(
-            self.root, text="Select Files", command=self.select_files
-        )
-        self.select_button.grid(row=0, column=0)
-
-        # Create a scrolled text area to display file names
-        self.text_area = scrolledtext.ScrolledText(self.root, width=100, height=10)
-        self.text_area.grid(row=1, column=0, sticky="nsew")
 
         # Configure grid to allow resizing
         self.root.grid_rowconfigure(1, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
-        # Position button at the bottom right of the text area
-        self.button_frame = tk.Frame(self.root)
-        self.button_frame.grid(row=2, column=0, sticky="se")
+        self.input_frame = self.addLabelFrame("Inputs", pady=(20, 0))
+        self.input_frame.Text("Select the PDFs to be processed")
+        self.input_frame.nextCol()
+        self.input_frame.AccentButton("Select Files", self.select_files, sticky="e")
 
-        # Create a button to copy the text area contents
-        self.copy_button = ttk.Button(
-            self.button_frame,
-            text="Copy Text",
-            command=self.copy_to_clipboard,
-            state=tk.DISABLED,
+        self.output_frame = self.addLabelFrame("Output", pady=(20, 0))
+        # Create a scrolled text area to display file names
+        self.text_area_frame = self.output_frame.addFrame("textarea", pady=(10, 0))
+        self.text_area = scrolledtext.ScrolledText(
+            self.text_area_frame.master, width=100, height=10
         )
-        self.copy_button.pack(side=tk.RIGHT)
+        self.text_area.grid(row=0, column=0, sticky="nsew")
+        self.output_frame.Button(
+            "Copy Text", self.copy_to_clipboard, args=("state", tk.DISABLED), sticky="e"
+        )
 
-        # Divider
-        self.status_divider = tk.Frame(self.root, height=2, bd=1, bg="grey")
-        self.status_divider.grid(row=3, column=0, sticky="ew")
-
-        # Label to show status
-        self.status_label = ttk.Label(self.root, text="Ready")
-        self.status_label.grid(row=4, column=0, sticky="se")
+        self.status_frame = self.addFrame("Status", pady=(10, 10))
+        self.status_label = ttk.Label(self.status_frame.master, text="Ready")
+        self.status_label.grid(row=0, column=0, sticky="e")
 
         # Queue for communication between threads
         self.queue = queue.Queue()
